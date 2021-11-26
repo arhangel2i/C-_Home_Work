482fa9d425687959a6b5055220156a48440d7ad5
arhangel2i <arhangel2i@mail.ru>
2021-11-26 18:36:44 +0300

    add Exmaple002 Array

---
+++ Lesson_02/Example002_Array/Program.cs
@@ -0,0 +1,16 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int retVal = arg1;
+    if (arg2 >retVal) retVal =arg2;
+    if (arg3 >retVal) retVal =arg3;
+    return retVal;
+}
+
+int[] array = {1,2,3,4,5,6,7,8,9};
+
+
+int max = Max(Max(array[0],array[1],array[2]),
+            Max(array[3],array[4],array[5]),
+            Max(array[6],array[7],array[8]));
+
+Console.WriteLine(max);
