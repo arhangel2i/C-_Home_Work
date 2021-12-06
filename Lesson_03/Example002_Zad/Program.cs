bacb9692c3d47250fefc6982416632f2285d40dd
arhangel2i <arhangel2i@mail.ru>
2021-12-06 19:11:39 +0300

    add Example002 zad

---
+++ Lesson_03/Example002_Zad/Program.cs
@@ -0,0 +1,17 @@
+﻿//===== Работа с текстом
+
+string ModifyString(string msg)
+{
+    string retStr = string.Empty;
+    for (int i =0; i<msg.Length; i++)
+    {
+        if (msg[i]=='к') retStr +='К';
+        else if (msg[i]==' ') retStr +='-';
+        else if (msg[i]=='С') retStr +='с';
+        else retStr +=msg[i];
+    }
+    return retStr;
+}
+
+string msg = "каркуши-Сакуши-МарсушиС";
+Console.Write(ModifyString(msg));
