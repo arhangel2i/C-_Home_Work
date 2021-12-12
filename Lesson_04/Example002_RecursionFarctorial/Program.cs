637bd74016f549a401b41725d07e682363dc16d3
arhangel2i <arhangel2i@mail.ru>
2021-12-12 16:24:29 +0300

    add Exmaple002 recursion factorial

---
+++ Lesson_04/Example002_RecursionFarctorial/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+for (int i = 1; i <= 40; i++)
+{
+    Console.WriteLine($"{i} = {Fibonacci(i)}");
+}
+
+
+double Factorial (double n)
+{
+    if (n==1) return 1;
+    else return n * Factorial(n-1);
+}
+
+double Fibonacci(double n)
+{
+    if (n==1 || n== 2) return 1;
+    else return Fibonacci(n-1)+Fibonacci(n-2);
+}
